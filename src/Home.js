252c08f900ebff611a72809ad8814cf34d2deddf
wnipuna <nipuna.r3@gmail.com>
2021-07-08 13:07:13 +0530

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,17 @@
-const Home = () =>{
-return (<div className="home">
-      <h2>Homepage</h2>
+const Home = () => {
+
+    const clickHandle = (e) => {
+        console.log('clicked', e);
+    }
+
+    const clickMeAgain = (name, e) => {
+        console.log('name ' + name, e);
+    }
+
+    return (<div className="home">
+        <h2>Homepage</h2>
+        <button onClick={clickHandle}>Click me</button>
+        <button onClick={(e) => clickMeAgain('nipuna', e)}>Click me again</button>
     </div>);
 }
 
